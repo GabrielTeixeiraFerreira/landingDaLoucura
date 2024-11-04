ddf272274b1ff20aea80b2ef73c72ef5cede7ecc
GabrielTeixeiraFerreira <157617731+GabrielTeixeiraFerreira@users.noreply.github.com>
2024-11-04 07:33:53 -0300

    landingDaLoucura V3
    pag de login refeita e classes de remover feitas

--- Khita_Servlet/src/main/java/com/khita_servlet/controller/tipoCurso/RemoverTipo.java
+++ Khita_Servlet/src/main/java/com/khita_servlet/controller/tipoCurso/RemoverTipo.java
@@ -1,5 +1,7 @@
 package com.khita_servlet.controller.tipoCurso;
 
+import com.khita_servlet.dao.TipoCursoDAO;
+import com.khita_servlet.model.TipoCurso;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,6 +14,38 @@ import java.io.IOException;
 public class RemoverTipo extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Instanciando o objeto modificador de BD
+        TipoCursoDAO tipoCursoDAO = new TipoCursoDAO();
+
+        // Pegando os parâmetros como String
+        String tipo = req.getParameter("tipo");
+
+        // Pegando o objeto do Banco
+        TipoCurso tipoCurso = tipoCursoDAO.buscarTipoCursoPeloTipo(tipo);
+
+        if (tipoCurso != null){
+            // Caso exista esse tipo de curso
+
+            if (tipoCursoDAO.removerTipoCurso(tipoCurso.getId())>0){
+                // Caso consiga remover
+                req.setAttribute("erro", false);
+                req.setAttribute("mensagem", "Tipo de curso removido com sucesso");
+
+            } else{
+                // Caso não consiga remover
+                req.setAttribute("erro", true);
+                req.setAttribute("mensagem", "Ocorreu um erro com o banco de dados ou esse tipo de curso está alocado a um curso.");
+            }
+
+        } else{
+            // Caso não possua esse tipo de curso no banco
+            req.setAttribute("erro", true);
+            req.setAttribute("mensagem", "Não existe esse tipo de curso");
+        }
+
+        // Mandando para a página com o resultado da ação
+        req.getRequestDispatcher("/pages/paginas-mensagem/area-oculta-mensagem.jsp").forward(req,resp);
 
     }
 }
