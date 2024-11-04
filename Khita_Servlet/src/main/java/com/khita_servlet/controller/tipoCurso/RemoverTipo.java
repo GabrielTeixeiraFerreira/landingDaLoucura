29eef12038030d58e864225b63b1da1c77a73582
Filipi Inácio Penha <filipi.santos@germinare.org.br>
2024-11-04 08:17:54 -0300

    Landing da loucura
    Muitas coisas...

--- Khita_Servlet/src/main/java/com/khita_servlet/controller/tipoCurso/RemoverTipo.java
+++ Khita_Servlet/src/main/java/com/khita_servlet/controller/tipoCurso/RemoverTipo.java
@@ -1,5 +1,8 @@
 package com.khita_servlet.controller.tipoCurso;
 
+import com.khita_servlet.dao.CursoDAO;
+import com.khita_servlet.dao.TipoCursoDAO;
+import com.khita_servlet.model.TipoCurso;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,6 +15,35 @@ import java.io.IOException;
 public class RemoverTipo extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TipoCursoDAO tipoDAO = new TipoCursoDAO();
+        CursoDAO cursoDAO = new CursoDAO();
 
+        String tipo = req.getParameter("tipo");
+
+        if (tipo != null) {
+            TipoCurso tipoCurso = new TipoCurso(tipo);
+
+            // Verificar se o tipoCurso está associado a algum curso
+            if (cursoDAO.isTipoCursoAlocado(tipo)) {
+                req.setAttribute("erro", true);
+                req.setAttribute("mensagem", "Tipo de curso não pode ser removido pois está associado a um ou mais cursos.");
+            } else {
+                // Remover o tipoCurso caso não esteja associado
+                if (tipoDAO.removerTipoCurso(tipoCurso.getId()) > 0) {
+                    req.setAttribute("erro", false);
+                    req.setAttribute("mensagem", "Tipo de curso removido com sucesso!");
+                } else {
+                    req.setAttribute("erro", true);
+                    req.setAttribute("mensagem", "Ocorreu um erro ao remover o tipo de curso, tente novamente.");
+                }
+            }
+        } else {
+            req.setAttribute("erro", true);
+            req.setAttribute("mensagem", "Tipo de curso não existente.");
+        }
+
+        req.getRequestDispatcher("/pages/paginas-mensagem/area-oculta-mensagem.jsp").forward(req, resp);
     }
+
 }
+
